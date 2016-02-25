1efb9c95d3d9a64d94d3087817233b551c3dca69
Lee Hesselden <on2@users.noreply.github.com>
2016-02-25 18:40:30 +0000

    Added ability to update the cache

--- public/index.php
+++ public/index.php
@@ -39,5 +39,21 @@ $builder = new \Imgix\UrlBuilder(getenv('IMGIX_DOMAIN'));
 $builder->setSignKey(getenv('IMGIX_SIGN_KEY'));
 $url = $builder->createURL($matches[4], $params);
 
-header($_SERVER['SERVER_PROTOCOL'] . ' 301 Moved Permanently');
-header('Location: ' . $url);
+$target = __DIR__ . '/' . $matches[1] . $matches[2] . 'x' . $matches[3] . '/' . $path;
+$dir = dirname($target);
+
+if (!is_dir($dir)) {
+    mkdir($dir, 0777, true);
+}
+
+$image = file_get_contents($url);
+$length = file_put_contents($target, $image);
+
+switch (pathinfo($target, PATHINFO_EXTENSION)) {
+    case "jpg" : $mime = "image/jpeg"; break;
+    case "png" : $mime = "image/png";  break;
+    case "gif" : $mime = "image/gif";  break;
+}
+
+header("Content-Type: " . $mime);
+echo $image;
